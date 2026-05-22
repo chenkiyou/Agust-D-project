2b769033afc8d5050f930e58e611ed0233ecf8a6
“agust-cm” <2792616328@qq.com>
2026-05-22 16:24:19 +0800

    init vue3 music archive project

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="app">
+    <h1>AGUSTD Music Archive</h1>
+    <p>项目创建成功，接下来开始开发首页。</p>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.app {
+  min-height: 100vh;
+  background: #050505;
+  color: #f5ead4;
+  padding: 40px;
+}
+
+h1 {
+  font-size: 56px;
+  margin-bottom: 16px;
+}
+</style>
