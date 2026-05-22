19e5ad2c91b84e61809f030390ec382a93781e0b
“agust-cm” <2792616328@qq.com>
2026-05-22 16:38:53 +0800

    feat: create home page

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,11 @@
 <template>
-  <div class="app">
-    <h1>AGUSTD Music Archive</h1>
-    <p>项目初始化成功，接下来开始开发首页。</p>
-  </div>
+  <Home />
 </template>
 
 <script setup>
+import Home from './views/Home.vue'
 </script>
 
-<style scoped>
-.app {
-  min-height: 100vh;
-  background: #050505;
-  color: #f5ead4;
-  padding: 40px;
-}
+<style>
 
-h1 {
-  font-size: 56px;
-  margin-bottom: 16px;
-}
-
-p {
-  font-size: 20px;
-  color: #c9b88f;
-}
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <main class="home-page">
+    <h1>AGUST D Music Archive</h1>
+    <p>首页页面创建成功，接下来开始还原 UI 设计稿。</p>
+  </main>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.home-page {
+  min-height: 100vh;
+  background: #050505;
+  color: #f5ead4;
+  padding: 40px;
+}
+
+h1 {
+  font-size: 56px;
+  margin-bottom: 16px;
+}
+
+p {
+  color: #c9b88f;
+  font-size: 20px;
+}
+</style>
